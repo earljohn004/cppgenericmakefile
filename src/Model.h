ffe2625b446b34f0bd07bc85e2fc6eb1f9333be6
Earl John Abaquita <abaquitaej@KSERVSDSIM1.ktdp.local>
2021-01-11 17:36:35 +0900

    sorted

--- src/Model.h
+++ src/Model.h
@@ -5,15 +5,17 @@
 #include <string>
 
 class Model{
-	private:
-		std::string first_name_;
-		std::string last_name_;
+
 	public:
 		Model();
 		Model(std::string fname, std::string lname);
 		~Model();
 		std::string get_first_name();
 		std::string get_last_name();
+
+	private:
+		std::string first_name_;
+		std::string last_name_;
 };
 
 #endif //_SRC_MODEL_H_
